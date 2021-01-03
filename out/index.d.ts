01c179f75a51966f97a30fc9bf0ed510863da00d
kosmo <kosmoziemia@gmail.com>
2021-01-03 15:39:29 +0100

    remove build from repo

--- out/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { YTPlaylist } from './interfaces';
-export = fetchFromPlaylist;
-/**
- * Scraps youtube playlist metadata and all its videos
- * @param url URL or ID of the playlist you want to scrap
- */
-declare function fetchFromPlaylist(url: string): Promise<YTPlaylist>;
-//# sourceMappingURL=index.d.ts.map
